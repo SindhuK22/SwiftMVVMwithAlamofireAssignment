f89eef23579f372a278befb46518c0ab6c21d0fc
SindhuK22 <144616373+SindhuK22@users.noreply.github.com>
2023-09-14 16:33:25 +0530

    Add files via upload

---
+++ MVVMWithAlamofire/MVVMWithAlamofire/MVVMWithAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMWithAlamofireApp.swift
+//  MVVMWithAlamofire
+//
+//  Created by Sindhu K on 11/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMWithAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ MVVMWithAlamofire/MVVMWithAlamofire/Model/TokenResponse.swift
@@ -0,0 +1,13 @@
+//
+//  TokenResponse.swift
+//  MVVMWithAlamofire
+//
+//  Created by Sindhu K on 11/09/23.
+//
+
+struct TokenResponse: Decodable {
+    let token: String
+    let expiresOn: String
+}
+
+ 
